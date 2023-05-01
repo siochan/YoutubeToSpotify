73042126b81da971abf0daed0cdc8f154b1bc582
siochan <sallyh6@uci.edu>
2023-05-01 01:02:19 -0700

    Create YoutubeToSpotify.py
    Scaffold for Program

---
+++ YoutubeToSpotify.py
@@ -0,0 +1,26 @@
+# Uses Youtube Data API, Spotify Web API, Youtube-DL Library
+
+class YoutubetoSpotify:
+    
+    def __init__(self):
+        pass
+    
+    # get youtube account
+    def get_youtube_client(self):
+        pass
+    
+    #get liked youtube videos
+    def get_liked_videos(self):
+        pass
+
+    # make new spotify playlist
+    def create_playlist(self):
+        pass
+    
+    # get spotify song ID
+    def get_spotify_uri(self):
+        pass
+
+    # add song to new spotify playlist
+    def add_song(self):
+        pass
