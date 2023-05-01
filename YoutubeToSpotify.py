73b8d8f346724c647827f3292ae1e597dd63c365
siochan <sallyh6@uci.edu>
2023-05-01 01:55:21 -0700

    Update YoutubeToSpotify.py
    Spotify API "create a playlist"

--- YoutubeToSpotify.py
+++ YoutubeToSpotify.py
@@ -1,9 +1,12 @@
 # Uses Youtube Data API, Spotify Web API, Youtube-DL Library
 
+import json
+import requests
+from secret import spotify_user_id, spotify_token
 class YoutubetoSpotify:
     
     def __init__(self):
-        pass
+        self.user_id = spotify_user_id
     
     # get youtube account
     def get_youtube_client(self):
@@ -15,7 +18,25 @@ class YoutubetoSpotify:
 
     # make new spotify playlist
     def create_playlist(self):
-        pass
+        request_body = json.dumps({
+            "name":"Youtube Liked Videos",
+            "description":"All Liked Youtube Videos",
+            "public":True
+        })
+
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(self.user_id)
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={
+                "Content-Type":"application/json",
+                "Authorization":"Bearer {}".format(spotify_token)
+            }
+        )
+        # playlist ID
+        response_json = response.json()
+
+        return response_json["id"]
     
     # get spotify song ID
     def get_spotify_uri(self):
