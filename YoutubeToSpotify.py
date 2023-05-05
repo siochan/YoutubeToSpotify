5b0051efa42e8f987527a95d55972054dae1481b
Sally Huynh <sallyh6@uci.edu>
2023-05-04 18:30:50 -0700

    Update YoutubeToSpotify.py
    Get Spotify URI

--- YoutubeToSpotify.py
+++ YoutubeToSpotify.py
@@ -18,8 +18,28 @@ class YoutubetoSpotify:
         pass
     
     # get spotify song ID
-    def get_spotify_uri(self):
-        pass
+    def get_spotify_uri(self, song_name, artist):
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+            song_name,
+            artist
+        )
+
+        response = requests.get(
+            query, 
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+
+        response_json = response.json()
+        songs = response_json["tracks"]["items"]
+
+        # get only first song
+        uri = songs[0]["uri"]
+
+        return uri
+
 
     # add song to new spotify playlist
     def add_song(self):
